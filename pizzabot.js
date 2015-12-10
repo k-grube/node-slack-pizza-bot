a8ab3c8a67dc61a5424976ef329b29d6e5dcbfb9
kevin <grube.kevin@gmail.com>
2015-12-09 19:27:43 -0800

    Add queue, cleanup comments, add error handling

--- pizzabot.js
+++ pizzabot.js
@@ -6,9 +6,15 @@ var Slack = require('slack-node'),
     Q = require('q'),
     pizzapi = require('dominos');
 
+var DEFAULT_UPDATE_TIMER = 60000;
+
 var apiToken = process.env.SLACK_TOKEN,
     slack = new Slack(apiToken);
 
+/**
+ *
+ * @type {{route: Function, track: Function, find: Function, queue: {add: pizzabot.queue.add, contains: pizzabot.queue.contains, start: pizzabot.queue.start, stop: pizzabot.queue.stop, remove: pizzabot.queue.remove}, usage: Function, send: Function}}
+ */
 var pizzabot = {
 
     /**
@@ -24,7 +30,7 @@ var pizzabot = {
      * @param body.command
      * @param body.text
      * @param body.response_url
-     * @returns {Promise<T>}
+     * @returns {*}
      */
     route: function (body) {
         var deferred = Q.defer();
@@ -32,7 +38,7 @@ var pizzabot = {
         var args = body.text.split(' ');
 
         if (args.length < 2) {
-            deferred.reject(pizzabot.usage(body));
+            deferred.reject(pizzabot.usage());
         } else {
             var command = args[0];
 
@@ -42,7 +48,7 @@ var pizzabot = {
                 });
 
             } else {
-                deferred.reject(pizzabot.usage(body));
+                deferred.reject(pizzabot.usage());
             }
         }
 
@@ -55,9 +61,10 @@ var pizzabot = {
             /**
              *
              * @param {{}} result
+             * @param {boolean} result.success
              * @param {{}} result.orders
-             * @param {[{}]} result.orders.OrderStatus
-             * @param {string} result.orders.OrderStatusDriverName
+             * @param {Object[]} result.orders.OrderStatus
+             * @param {string} result.orders.OrderStatus.DriverName
              * @param {string} result.orders.OrderStatus.OrderDescription
              * @param {string} result.orders.OrderStatus.OvenTime
              * @param {string} result.orders.OrderStatus.StartTime
@@ -68,23 +75,34 @@ var pizzabot = {
              * @param {string} result.query.Phone
              */
             function (result) {
-                //order returned
-                if (result.orders.OrderStatus.length > 0) {
-                    var pizzaStatus = "";
-                    if (result.orders.OrderStatus.length == 1) {
-                        pizzaStatus = "Order status: " + result.orders.OrderStatus[0].OrderStatus + "\r\n";
-                        pizzaStatus += "Order description: " + result.orders.OrderStatus[0].OrderDescription + "\r\n";
-                    } else {
-                        for (var i = 0; i < result.orders.OrderStatus; i++) {
-                            pizzaStatus += "Order #" + (i + 1) + " - status: " + result.orders.OrderStatus[i].OrderStatus + "\r\n";
-                            pizzaStatus += "Order #" + (i + 1) + " - description: " + result.orders.OrderStatus[i].OrderDescription + "\r\n";
-                        }
-                    }
+                console.log(result);
+                if (result.success) {
+                    //order returned
+                    if (result.orders.OrderStatus.length > 0) {
+                        var pizzaStatus = "";
+                        for (var i = 0; i < result.orders.OrderStatus.length; i++) {
+                            var orderStatus = result.orders.OrderStatus[i].OrderStatus;
+                            pizzaStatus += "Order #" + (i + 1) + " - status: " + orderStatus + "\r\n";
+                            pizzaStatus += "Order #" + (i + 1) + " - description: " + orderStatus + "\r\n";
 
-                    callback({
-                        text: pizzaStatus,
-                        response_type: "in_channel"
-                    });
+                            if (!pizzabot.queue.contains(phone) && orderStatus !== 'Completed') {
+                                pizzabot.queue.add(phone, body, orderStatus);
+                            }
+                            if (pizzabot.queue.contains(phone) && orderStatus === 'Completed') {
+                                pizzabot.queue.remove(phone);
+                            }
+                        }
+
+                        callback({
+                            text: pizzaStatus,
+                            response_type: "in_channel"
+                        });
+                    } else {
+                        callback({
+                            text: "No orders found.",
+                            response_type: "in_channel"
+                        });
+                    }
                 } else {
                     callback({
                         text: "No orders found.",
@@ -118,6 +136,7 @@ var pizzabot = {
                     if (results.result.Stores.length > 0) {
                         var message = "```Best Match:"
                             + "\r\nStoreID: " + results.result.Stores[0].StoreID
+                            + "\r\nPhone: " + results.result.Stores[0].Phone
                             + "\r\nAddress: " + results.result.Stores[0].AddressDescription
                             + "\r\nOpen for delivery? " + results.result.Stores[0].ServiceIsOpen.Delivery
                             + "```";
@@ -144,35 +163,129 @@ var pizzabot = {
         );
     },
 
-    queue: function () {
+    /**
+     * queue singleton, not a queue in the data structure sense.
+     * queues the tracked orders so updates can be sent back to slack.
+     */
+    queue: (/**
+     *
+     * @returns {{add: pizzabot.queue.add, contains: pizzabot.queue.contains, start: pizzabot.queue.start, stop: pizzabot.queue.stop, remove: pizzabot.queue.remove}}
+     */
+        function () {
+        var self = this;
+        self.queue = {};
 
-    },
+        return {
+            /**
+             * Add the order to the queue
+             * @param phone
+             * @param body
+             * @param lastStatus
+             */
+            add: function (phone, body, lastStatus) {
+                self.queue[phone] = {
+                    body: body,
+                    phone: phone,
+                    lastStatus: lastStatus
+                };
+                console.log('add queue', phone);
+                self.stop();
+                self.start();
+            },
+            /**
+             * Is the order being tracked?
+             * @param phone
+             * @returns {boolean}
+             */
+            contains: function (phone) {
+                return typeof self.queue[phone] !== 'undefined';
+            },
+            /**
+             * start intervals to periodically check the API for updates
+             */
+            start: function () {
+                pizzabot.queue.stop();
+                for (var q in self.queue) {
+                    if (self.queue.hasOwnProperty(q)) {
+                        self.queue[q].interval = setInterval(function (t) {
+                            pizzapi.Track.byPhone(t.phone, function (result) {
+                                if(result.success) {
+                                    if (result.orders.OrderStatus.length > 0) {
 
-    usage: function (body) {
+                                    } else {
+
+                                    }
+                                }else{
+
+                                }
+                            });
+                        }, DEFAULT_UPDATE_TIMER, self.queue[q]);
+                    }
+                }
+            },
+            /**
+             * clear all intervals
+             */
+            stop: function () {
+                for (var i in self.queue) {
+                    if (self.queue.hasOwnProperty(i)) {
+                        clearInterval(self.queue[i].interval);
+                    }
+                }
+                console.log('intervals cleared');
+            },
+            /**
+             * remove order from queue
+             * @param phone
+             */
+            remove: function (phone) {
+                delete self.queue[phone];
+                self.stop();
+                self.start();
+                console.log('remove queue', phone);
+            }
+        }
+    })(),
+
+    /**
+     * generate usage string
+     * @returns {{text: string, response_type: string}}
+     */
+    usage: function () {
         var message = {
             text: "```/pizzabot [command: track, find, order] [arg]\r\n" +
-            "track [phone number] - in the format of 1234567890[+000]\r\n" +
-            "where 123 is area code, 4567890 is the phone number and 000 is the extension\r\n" +
-            "find [zip] - in the format of 12345\r\n" +
-            "Locates the nearest store to the zip specified```",
+            "  track [phone number] - in the format of 1234567890[+000]\r\n" +
+            "    where 123 is area code, 4567890 is the phone number and 000 is the extension\r\n" +
+            "  find [zip] - in the format of 12345\r\n" +
+            "    locates the nearest store to the zip specified```",
             response_type: "ephemeral"
         };
         return message;
-        //pizzabot.send(body, message);
     },
 
+    /**
+     * send message via POST from slash command to channel
+     * @param body
+     * @param message
+     */
     send: function (body, message) {
         message.username = 'pizzabot';
 
         request.post(body.response_url, message, function (err, httpResponse, body) {
-            console.log('err', err, 'httpResponse', httpResponse, 'body', body);
+            console.log('pizzabot send', 'err', err, 'httpResponse', httpResponse, 'body', body);
+        });
+    },
+    /**
+     * use slack-node API to send message, requires SLACK_TOKEN env variable be set
+     * @param body
+     * @param message
+     */
+    sendAPI: function (body, message) {
+
+        slack.api('chat.postMessage', message, function (err, response) {
+            console.log('pizzabot sendAPI', 'err', err, 'response', response);
         });
 
-        /*
-         slack.api('chat.postMessage', message, function (err, response) {
-         console.log('err', err, 'response', response);
-         });
-         */
     }
 };
 
