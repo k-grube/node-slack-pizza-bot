e6da2f1c367908fcfed7e3c0c41c3df4a5d43d49
Kevin Grube <grube.kevin@gmail.com>
2016-07-22 11:29:16 -0700

    Exclude Abandoned orders from display.

--- pizzabot.js
+++ pizzabot.js
@@ -93,8 +93,10 @@ var pizzabot = {
                         for (var i = 0; i < result.orders.OrderStatus.length; i++) {
                             var orderStatus = result.orders.OrderStatus[i].OrderStatus,
                                 orderDescription = result.orders.OrderStatus[i].OrderDescription;
-                            pizzaStatus += "Order #" + (i + 1) + " - status: " + orderStatus + "\r\n";
-                            pizzaStatus += "Order #" + (i + 1) + " - description: " + orderDescription + "\r\n";
+                            if(orderStatus !== 'Abandoned') {
+                                pizzaStatus += "Order #" + (i + 1) + " - status: " + orderStatus + "\r\n";
+                                pizzaStatus += "Order #" + (i + 1) + " - description: " + orderDescription + "\r\n";
+                            }
 
 
                             if (!pizzabot.queue.contains(phone) && orderStatus !== 'Completed') {
